6bef974efb77057c7860241dd743a98713242850
Sachikoo7 <118894316+Sachikoo7@users.noreply.github.com>
2022-12-26 18:55:38 +0100

    add functions euclidean_norm and argmin

--- function.h
+++ function.h
@@ -10,4 +10,8 @@ void find_k_first_primes(int size, mpz_t *sievePrimeList);
 
 double correlation_coeff(double *X, double *Y, int size);
 
+double euclidean_norm(double *vector, int size);
+
+unsigned int argmin(double *L, int size);
+
 #endif //DLC_SIDECHANNEL_ATTACK_ON_PRIME_GENERATION_FUNCTION_H
