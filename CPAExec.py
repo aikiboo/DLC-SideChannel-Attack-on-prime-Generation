46ef2d528bfe9c876e4d891846f2e5a56e73087b
florian <florian.suchaud @hotmail.fr>
2023-01-10 15:40:46 +0100

    update script

--- CPAExec.py
+++ CPAExec.py
@@ -3,7 +3,8 @@ import time
 
 total = 0
 correct = 0
-
+failLess100Cand = 0
+errorPair = 0
 while (total < 1000):
     NBsieve = 53
     sizePremier = 256
@@ -12,7 +13,6 @@ while (total < 1000):
     lines = output.split('\n')
     nbCand = lines[1].split(': ')[1]
     premier = lines[2].split('\t')[1]
-
     stream = os.popen(f'./analyse output_hamming_with_func_and_noise.txt {NBsieve} {nbCand}')
     output = stream.read()
     lines = output.split('\n')
@@ -21,8 +21,15 @@ while (total < 1000):
     except:
         continue
     total = total + 1
-    print(f" {premier} vs {premier2}")
+    print(f" {premier} vs {premier2} ({nbCand})")
     if (int(premier) == int(premier2)):
         correct += 1
-    print(f"{correct} / {total}")
+
+    else:
+        if int(nbCand) < 100:
+            failLess100Cand += 1
+        if int(premier2) % 2 == 0:
+            errorPair += 1
+
+    print(f"{correct} / {total}    ({total - correct - failLess100Cand})  ({errorPair})")
     time.sleep(1)
