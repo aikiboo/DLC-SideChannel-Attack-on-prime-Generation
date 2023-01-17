746b04cd1d7b415d68d7f62585f28887a239b863
Sachikoo7 <118894316+Sachikoo7@users.noreply.github.com>
2023-01-17 21:57:57 +0100

    add the module

--- SPA/genSeq.c
+++ SPA/genSeq.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include "../common/function.h"
 
-void main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[]) {
 
     if (argc < 3) {
         printf("Usage : ./genSeq [nbPrimeInSieve] [sizeOfGeneratedRandom] \n");
@@ -13,41 +13,70 @@ void main(int argc, char const *argv[]) {
     int nbSievePrimes = atoi(argv[1]);
     int size = atoi(argv[2]);
     srand(time(NULL));
+    mpz_t p;
+    mpz_t q;
+    mpz_t N;
+    mpz_inits(p,q,N,NULL);
 
     //Setup de la sortie
-    FILE *output = fopen("output_sequence.txt", "w");
+    FILE *output_p = fopen("output_sequence_p.txt", "w");
+    FILE *output_q = fopen("output_sequence_q.txt", "w");
+    FILE *module = fopen("module_rsa.txt","w");
 
-    //setup du gen random
+    //Setup du gen random
     gmp_randstate_t randstate;
     gmp_randinit_default(randstate);
     gmp_randseed_ui(randstate, time(NULL));
 
-    //setup du sieve
+    //Setup du sieve
     mpz_t * sievePrimeList = malloc(sizeof(mpz_t) * nbSievePrimes);
     find_k_first_primes(nbSievePrimes, sievePrimeList);
-    //Génération du premier entier impair
-    mpz_t tmpRand;
-    gen_k_bits_number_odd(tmpRand, size, randstate);
-    int isPrime;
-    while(1){
-        isPrime = 0;
-        for(int i =0;i<nbSievePrimes;i++){
-            fprintf(output,"c");
-            if(mpz_divisible_p(tmpRand,sievePrimeList[i])){
-                isPrime =1;
-                break;
-            }
-        }
-        //si il a un diviseur dans le sieve
-        if(isPrime==0){
-            fprintf(output,"a");
-            if(mpz_probab_prime_p(tmpRand, 10))break;
 
+    for (int j = 0; j < 2; j++) {
+        //Génération du premier entier impair
+        mpz_t tmpRand;
+        gen_k_bits_number_odd(tmpRand, size, randstate);
+        int isPrime;
+        while(1){
+            isPrime = 0;
+            for(int i =0;i<nbSievePrimes;i++){
+                if (j == 0) fprintf(output_p,"c");
+                else fprintf(output_q,"c");
+                
+                if(mpz_divisible_p(tmpRand,sievePrimeList[i])){
+                    isPrime =1;
+                    break;
+                }
+            }
+            //S'il y a un diviseur dans le sieve
+            if(isPrime==0){
+                if (j == 0) fprintf(output_p,"a");
+                else fprintf(output_q,"a");
+                if(mpz_probab_prime_p(tmpRand, 10))break;
+
+            }
+            if (j == 0) fprintf(output_p,"b");
+            else fprintf(output_q,"b");
+            mpz_add_ui(tmpRand,tmpRand,2);
         }
-        fprintf(output,"b");
-        mpz_add_ui(tmpRand,tmpRand,2);
+
+        printf("Is prime (1=prob,2=100\%) : %d\n", mpz_probab_prime_p(tmpRand, 10));
+        if (j == 0) {
+            mpz_set(p,tmpRand);
+            gmp_printf("Premier p : \t%Zu\n", p);
+        }
+        else {
+            mpz_set(q,tmpRand);
+            gmp_printf("Premier q : \t%Zu\n", q);
+        }
+       
+
     }
-    fclose(output);
-    printf("Is prime (1=prob,2=100\%) : %d\n", mpz_probab_prime_p(tmpRand, 10));
-    gmp_printf("Premier: \t%Zu\n", tmpRand);
+    fclose(output_p);
+    fclose(output_q);
+
+    //Calcul du module
+    mpz_mul(N,p,q);
+    gmp_fprintf(module,"%Zd",N);
+    
 }
