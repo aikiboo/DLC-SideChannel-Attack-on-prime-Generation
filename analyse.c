11e1bc397bd068cb9dab8a69ac74300f3ac8dd58
florian <florian.suchaud@hotmail.fr>
2023-01-25 14:50:56 +0100

    free mesures

--- analyse.c
+++ analyse.c
@@ -165,5 +165,7 @@ int main(int argc, char const *argv[]) {
     }
     chinese_remainder_theorem_cpa(p, sievePrimeList, candidats, nbSievePrimes);
     gmp_printf("p = %Zd\n", p);
-
+    for (int i = 0; i < nb_candidats; i++)
+        free(mesures[i]);
+    free(mesures);
 }
